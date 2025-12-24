f94d3f0b7bbd5777b658a4208f903ec8e903a4de
neelnevatia-afk <neelnevatia@gmail.com>
2025-12-24 21:35:58 +0530

    (ctrl.enter

---
+++ Lesson 4/H.W.py
@@ -0,0 +1,11 @@
+
+friends_birthdays = {
+    "Amit": "12th January",
+    "Sneha": "5th March",
+    "Ravi": "20th June",
+    "Priya": "15th August",
+    "Karan": "30th November"
+}
+
+
+print("Raj asked his friends about their birthdays. Here are the details:\n")
